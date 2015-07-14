e760aa90c92a333eac601627c1207413303b50b2
Srikanth <speruri@ciphercloud.com>
2015-07-14 21:04:24 +0530

    more changes

--- src/com/speruri/git/practice/HelloWorld.java
+++ src/com/speruri/git/practice/HelloWorld.java
@@ -3,5 +3,6 @@ package com.speruri.git.practice;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World, this is my GIT tutorial");
+		System.out.println("Saving it again");
 	}
 }
